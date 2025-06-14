767e3f2f27635063564d5a7158415bb5655b78d9
Manya Singh <manya426singh@gmail.com>
2025-06-14 08:06:39 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 from setuptools import find_packages,setup
-
+from typing import List
+HYPHEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''this func will return the list of requirements'''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+            
+    return requirements
 
 
 setup(
@@ -8,5 +20,5 @@ setup(
     author='Manya',
     author_mail='manya426singh@gmail.com',
     packages=find_packages(),
-    insatall_requires=['pandas','numpy','seaborn']
+    insatall_requires=get_requirements('requirements.txt')
 )
